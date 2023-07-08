0dd033b2b04cac48e857615773633fe312971b0f
RatInChat <vishyfishyalt@gmail.com>
2023-07-08 20:47:15 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // import required modules
-import discord from 'discord.js';
-import express from 'express';
-import http from 'http';
-import multer from 'multer';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import axios from 'axios';
-import dotenv from 'dotenv';
+import discord from "discord.js";
+import express from "express";
+import http from "http";
+import multer from "multer";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import axios from "axios";
+import dotenv from "dotenv";
 dotenv.config();
 // Set up database
-import Database from 'better-sqlite3';
-const db = new Database('database.db');
+import Database from "better-sqlite3";
+const db = new Database("database.db");
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS files (
@@ -24,11 +24,11 @@ db.exec(`
 
 // Set up Discord bot
 const bot = new discord.Client({
-  intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
+  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
 });
 
-bot.on('ready', async () => {
-  console.log('Bot is ready');
+bot.on("ready", async () => {
+  console.log("Bot is ready");
   // Fetch uploaded files on server startup
   await fetchUploadedFiles();
 });
@@ -41,13 +41,20 @@ const server = http.createServer(app);
 
 // Set up multer storage for file upload
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: Infinity }, // Set the file size limit to Infinity
+});
 
 // Array to store uploaded files
 const uploadedFiles = [];
 const chunkSize = 25 * 1024 * 1024; // Define the chunk size here
 async function fetchUploadedFiles() {
-  const files = db.prepare('SELECT DISTINCT name, messageId, chunkIndex, channelId FROM files').all();
+  const files = db
+    .prepare(
+      "SELECT DISTINCT name, messageId, chunkIndex, channelId FROM files"
+    )
+    .all();
 
   uploadedFiles.length = 0; // Clear the array
 
@@ -63,7 +70,9 @@ async function fetchUploadedFiles() {
 
       if (chunkIndex !== null) {
         // Split file, find all chunks for the file
-        const chunks = db.prepare('SELECT messageId FROM files WHERE name = ?').all(name);
+        const chunks = db
+          .prepare("SELECT messageId FROM files WHERE name = ?")
+          .all(name);
 
         if (chunks.length === 0) {
           console.error(`Chunks not found for file: ${name}`);
@@ -165,11 +174,11 @@ function generateFilesHTML() {
         `;
       }
     })
-    .join('');
+    .join("");
 }
 
 // Index route
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   // Fetch uploaded files on page load
   await fetchUploadedFiles();
 
@@ -222,12 +231,12 @@ app.get('/', async (req, res) => {
   res.send(indexHTML);
 });
 
-app.get('/download/:messageId', async (req, res) => {
+app.get("/download/:messageId", async (req, res) => {
   const { messageId } = req.params;
 
   const file = uploadedFiles.find((file) => file.messageId === messageId);
   if (!file) {
-    res.status(404).send('File not found');
+    res.status(404).send("File not found");
     return;
   }
 
@@ -236,7 +245,7 @@ app.get('/download/:messageId', async (req, res) => {
   try {
     const channel = await bot.channels.fetch(channelId, { force: true });
     if (!channel) {
-      res.status(500).send('Failed to fetch channel');
+      res.status(500).send("Failed to fetch channel");
       return;
     }
 
@@ -244,7 +253,9 @@ app.get('/download/:messageId', async (req, res) => {
       // Split file, download merged file
       const buffers = [];
       for (const attachment of attachments) {
-        const response = await axios.get(attachment.url, { responseType: 'arraybuffer' });
+        const response = await axios.get(attachment.url, {
+          responseType: "arraybuffer",
+        });
         const fileBuffer = Buffer.from(response.data);
         buffers.push(fileBuffer);
       }
@@ -252,43 +263,57 @@ app.get('/download/:messageId', async (req, res) => {
       const mergedBuffer = Buffer.concat(buffers);
 
       const attachment = attachments[0];
-      res.setHeader('Content-Type', attachment.contentType || 'application/octet-stream');
-      res.setHeader('Content-Disposition', `attachment; filename="${attachment.name}"`);
-      res.setHeader('Content-Length', mergedBuffer.length);
+      res.setHeader(
+        "Content-Type",
+        attachment.contentType || "application/octet-stream"
+      );
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${attachment.name}"`
+      );
+      res.setHeader("Content-Length", mergedBuffer.length);
       res.send(mergedBuffer);
     } else {
       // Non-split file, download single file
       const message = await channel.messages.fetch(messageId);
       if (!message) {
-        res.status(500).send('Message not found');
+        res.status(500).send("Message not found");
         return;
       }
 
       const attachment = message.attachments.first();
       if (!attachment) {
-        res.status(500).send('Attachment not found');
+        res.status(500).send("Attachment not found");
         return;
       }
-      const response = await axios.get(attachment.url, { responseType: 'arraybuffer' });
+      const response = await axios.get(attachment.url, {
+        responseType: "arraybuffer",
+      });
       const fileBuffer = Buffer.from(response.data);
 
-      res.setHeader('Content-Type', attachment.contentType || 'application/octet-stream');
-      res.setHeader('Content-Disposition', `attachment; filename="${attachment.name}"`);
-      res.setHeader('Content-Length', fileBuffer.length);
+      res.setHeader(
+        "Content-Type",
+        attachment.contentType || "application/octet-stream"
+      );
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${attachment.name}"`
+      );
+      res.setHeader("Content-Length", fileBuffer.length);
       res.send(fileBuffer);
     }
   } catch (error) {
-    console.error('Failed to fetch attachment:', error);
-    res.status(500).send('Failed to fetch attachment');
+    console.error("Failed to fetch attachment:", error);
+    res.status(500).send("Failed to fetch attachment");
   }
 });
 
-app.get('/download-merged/:messageId', async (req, res) => {
+app.get("/download-merged/:messageId", async (req, res) => {
   const { messageId } = req.params;
 
   const file = uploadedFiles.find((file) => file.messageId === messageId);
   if (!file) {
-    res.status(404).send('File not found');
+    res.status(404).send("File not found");
     return;
   }
 
@@ -297,7 +322,7 @@ app.get('/download-merged/:messageId', async (req, res) => {
   try {
     const channel = await bot.channels.fetch(channelId, { force: true });
     if (!channel) {
-      res.status(500).send('Failed to fetch channel');
+      res.status(500).send("Failed to fetch channel");
       return;
     }
 
@@ -305,7 +330,9 @@ app.get('/download-merged/:messageId', async (req, res) => {
       // Split file, download merged file
       const buffers = [];
       for (const attachment of attachments) {
-        const response = await axios.get(attachment.url, { responseType: 'arraybuffer' });
+        const response = await axios.get(attachment.url, {
+          responseType: "arraybuffer",
+        });
         const fileBuffer = Buffer.from(response.data);
         buffers.push(fileBuffer);
       }
@@ -313,40 +340,54 @@ app.get('/download-merged/:messageId', async (req, res) => {
       const mergedBuffer = Buffer.concat(buffers);
 
       const attachment = attachments[0];
-      res.setHeader('Content-Type', attachment.contentType || 'application/octet-stream');
-      res.setHeader('Content-Disposition', `attachment; filename="${attachment.name}"`);
-      res.setHeader('Content-Length', mergedBuffer.length);
+      res.setHeader(
+        "Content-Type",
+        attachment.contentType || "application/octet-stream"
+      );
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${attachment.name}"`
+      );
+      res.setHeader("Content-Length", mergedBuffer.length);
       res.send(mergedBuffer);
     } else {
-      res.status(400).send('File is not split');
+      res.status(400).send("File is not split");
     }
   } catch (error) {
-    console.error('Failed to fetch attachment:', error);
-    res.status(500).send('Failed to fetch attachment');
+    console.error("Failed to fetch attachment:", error);
+    res.status(500).send("Failed to fetch attachment");
   }
 });
 
-app.post('/delete', express.json(), async (req, res) => {
+app.post("/delete", express.json(), async (req, res) => {
   const { fileName } = req.body;
 
   if (fileName) {
     try {
-      const files = db.prepare('SELECT messageId, channelId, chunkIndex FROM files WHERE name = ?').all(fileName);
+      const files = db
+        .prepare(
+          "SELECT messageId, channelId, chunkIndex FROM files WHERE name = ?"
+        )
+        .all(fileName);
 
       for (const file of files) {
         const { messageId, channelId, chunkIndex } = file;
 
         const channel = await bot.channels.fetch(channelId, { force: true });
         if (!channel) {
-          console.error('Channel not found');
-          res.status(500).send('Channel not found');
+          console.error("Channel not found");
+          res.status(500).send("Channel not found");
           return;
         }
 
         try {
           if (chunkIndex !== null) {
             // Split file, delete all chunks
-            const chunks = db.prepare('SELECT messageId FROM files WHERE name = ? AND chunkIndex IS NOT NULL').all(fileName);
+            const chunks = db
+              .prepare(
+                "SELECT messageId FROM files WHERE name = ? AND chunkIndex IS NOT NULL"
+              )
+              .all(fileName);
 
             for (const chunk of chunks) {
               const { messageId } = chunk;
@@ -355,7 +396,9 @@ app.post('/delete', express.json(), async (req, res) => {
                 await message.delete();
               }
               // Delete the chunk from the database
-              db.prepare('DELETE FROM files WHERE messageId = ?').run(messageId);
+              db.prepare("DELETE FROM files WHERE messageId = ?").run(
+                messageId
+              );
             }
           } else {
             // Non-split file, delete the message directly
@@ -364,10 +407,10 @@ app.post('/delete', express.json(), async (req, res) => {
               await message.delete();
             }
             // Delete the message from the database
-            db.prepare('DELETE FROM files WHERE messageId = ?').run(messageId);
+            db.prepare("DELETE FROM files WHERE messageId = ?").run(messageId);
           }
         } catch (error) {
-          console.error('Failed to delete message:', messageId, error);
+          console.error("Failed to delete message:", messageId, error);
         }
       }
 
@@ -376,7 +419,7 @@ app.post('/delete', express.json(), async (req, res) => {
 
       res.sendStatus(200);
     } catch (error) {
-      console.error('Failed to delete file:', fileName, error);
+      console.error("Failed to delete file:", fileName, error);
       res.sendStatus(500);
     }
   } else {
@@ -384,17 +427,17 @@ app.post('/delete', express.json(), async (req, res) => {
   }
 });
 
-app.post('/upload', upload.single('file'), async (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
   const file = req.file;
   const fileName = file.originalname;
   const fileType = file.mimetype;
-  const channelId = '1071697670156066837'; // Replace with your desired channel ID
+  const channelId = "1071697670156066837"; // Replace with your desired channel ID
 
   try {
     const channel = await bot.channels.fetch(channelId);
     if (!channel) {
-      console.error('Channel not found');
-      res.status(500).send('Channel not found');
+      console.error("Channel not found");
+      res.status(500).send("Channel not found");
       return;
     }
 
@@ -409,11 +452,9 @@ app.post('/upload', upload.single('file'), async (req, res) => {
         ],
       });
 
-      db.prepare('INSERT INTO files (name, messageId, channelId) VALUES (?, ?, ?)').run(
-        fileName,
-        message.id,
-        channelId
-      );
+      db.prepare(
+        "INSERT INTO files (name, messageId, channelId) VALUES (?, ?, ?)"
+      ).run(fileName, message.id, channelId);
     } else {
       // Split file, store message IDs for each chunk
       const chunks = [];
@@ -428,7 +469,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
           files: [
             {
               attachment: chunk,
-              name: `${fileName}_part${i}.${fileType.split('/')[1]}`, // Unique name for each chunk
+              name: `${fileName}_part${i}.${fileType.split("/")[1]}`, // Unique name for each chunk
             },
           ],
         });
@@ -438,21 +479,21 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
       chunks.forEach((messageId, chunkIndex) => {
         db.prepare(
-          'INSERT INTO files (name, messageId, chunkIndex, channelId) VALUES (?, ?, ?, ?)'
+          "INSERT INTO files (name, messageId, chunkIndex, channelId) VALUES (?, ?, ?, ?)"
         ).run(fileName, messageId, chunkIndex, channelId);
       });
     }
 
     await fetchUploadedFiles();
 
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
-    console.error('Failed to upload file:', error);
-    res.status(500).send('Failed to upload file');
+    console.error("Failed to upload file:", error);
+    res.status(500).send("Failed to upload file");
   }
 });
 
 // Listen on port 3000
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
